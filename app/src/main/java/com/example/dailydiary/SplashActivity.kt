b74cc4eeab1a5ae307f0ff0d6c379c7b718745a4
Samuel Hussain <samuelhussain@live.com>
2022-03-27 22:20:13 -0700

    progress

---
+++ app/src/main/java/com/example/dailydiary/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.dailydiary
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+        val appIcon = findViewById<ImageView>(R.id.appIcon)
+        appIcon.alpha = 0f
+        appIcon.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
