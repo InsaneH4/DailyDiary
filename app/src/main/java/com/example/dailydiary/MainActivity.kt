26532bc627edd0367bf584162c5ec772c780aa14
Samuel Hussain <samuelhussain@live.com>
2022-03-30 16:17:34 -0700

    bottom navigation bar

--- app/src/main/java/com/example/dailydiary/MainActivity.kt
+++ app/src/main/java/com/example/dailydiary/MainActivity.kt
@@ -1,52 +1,32 @@
 package com.example.dailydiary
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.dailydiary.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationView
+import androidx.fragment.app.Fragment
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(), UpdateAndDelete {
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.appBarMain.toolbar)
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(
-                R.id.nav_host_fragment_content_main
-            ) as NavHostFragment
-        val navController = navHostFragment.navController
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.tasksFragment, R.id.contactsFragment), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setContentView(R.layout.activity_main)
+        makeCurrentFragment(TasksFragment())
+        bottom_navigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.tasks_item -> makeCurrentFragment(TasksFragment())
+                R.id.contacts_item -> makeCurrentFragment(ContactsFragment())
+                R.id.settings_item -> makeCurrentFragment(SettingsFragment())
+            }
+            true
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
     }
 
     override fun modifyItem(itemId: String?, isDone: Boolean?) {
