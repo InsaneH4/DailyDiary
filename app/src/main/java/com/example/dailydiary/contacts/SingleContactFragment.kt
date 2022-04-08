dd89b902fe4b88fa832b563ceeab478eb26ecb0c
Samuel Hussain <samuelhussain@live.com>
2022-04-07 22:54:34 -0700

    Contacts fragment progress

--- app/src/main/java/com/example/dailydiary/contacts/SingleContactFragment.kt
+++ app/src/main/java/com/example/dailydiary/contacts/SingleContactFragment.kt
@@ -1,15 +1,27 @@
 package com.example.dailydiary.contacts
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import com.example.dailydiary.other.MainActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import com.example.dailydiary.R
+import com.example.dailydiary.other.MainActivity
+import kotlinx.android.synthetic.main.fragment_single_contact.*
 
 class SingleContactFragment : Fragment() {
+    private val getResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            ContactPhoto.setImageURI(it.data?.data)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,10 +31,16 @@ class SingleContactFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val backButton = view.findViewById<Button>(R.id.backButton)
         val mainActivityView = activity as MainActivity
-        backButton?.setOnClickListener {
+        CancelButton?.setOnClickListener {
             mainActivityView.makeCurrentFragment(ContactsFragment())
+        }
+        SaveButton?.setOnClickListener {
+            mainActivityView.makeCurrentFragment(ContactsFragment())
+        }
+        ChooseImageBtn?.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            getResult.launch(intent)
         }
     }
 }
